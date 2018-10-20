099882468bee6a4c382e1f5f5272d2eb5dca5e84
Rafal Musial <rafal.musial@qnt.pl>
2018-10-20 20:41:46 +0200

    project start

--- src/main/java/pl/creator/pdf/Build.java
+++ src/main/java/pl/creator/pdf/Build.java
@@ -1,7 +1,34 @@
 package pl.creator.pdf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 /**
  * @author rafal.musial
  */
 public class Build {
+
+    private Logger logger = LoggerFactory.getLogger(Build.class);
+
+    private byte[] pdfContent;
+
+    private void generateTOFile(String pathToFile) {
+        logger.info("Generate pdf file");
+        try {
+            // Create file
+            FileWriter fstream = new FileWriter(pathToFile);
+            BufferedWriter out = new BufferedWriter(fstream);
+            if (pdfContent != null)
+                for (Byte b : pdfContent) {
+                    out.write(b);
+                }
+            out.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
 }
